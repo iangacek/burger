eabb5f311e621a34ba23e96d3c3e656b38c4e540
Ian Gacek <gacek004@umn.edu>
2019-07-21 21:21:31 -0500

    Updated schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('California Burger');
+INSERT INTO burgers (name) VALUES ('Rodeo Burger');
+INSERT INTO burgers (name, is_eaten) VALUES ('Black and Blue Burger', true);
+INSERT INTO burgers (name, is_eaten) VALUES ('The Parlour Burger', true);
+INSERT INTO burgers (name, is_eaten) VALUES ('Jucy Lucy', true);
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
