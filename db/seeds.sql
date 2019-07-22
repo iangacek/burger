30d86ab702ea8797dfa7f0d9cae87690f8fd7a1f
Ian Gacek <48931196+iangacek@users.noreply.github.com>
2019-07-22 13:21:08 -0500

    Updated schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Bison Burger');
+INSERT INTO burgers (name) VALUES ('Black and Bleu Burger');
+INSERT INTO burgers (name) VALUES ('California Burger');
+INSERT INTO burgers (name) VALUES ('Green Chile Burger');
+INSERT INTO burgers (name) VALUES ('Breakfast Burger');
